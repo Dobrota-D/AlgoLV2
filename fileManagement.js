a96a93e5a6704eb81ed5db4cda073391dc29475a
Maxime Luchini <Maxime.luchini@edu.itescia.fr>
2021-01-28 11:51:39 +0100

    Story 1 done

--- fileManagement.js
+++ fileManagement.js
@@ -1,32 +1,42 @@
 let fs = require('fs');
 
-function toDateTime(secs) {
-  var t = new Date(1970); // Epoch
-  t.setTime(secs * 1000);
-  return t;
+// déclaration de variables
+let args = process.argv
+let inputFile
+let outputFile
+
+// Boucle permettant de vérifier la présence de -action transform et de récupérer le chemin d'input et d'output dans les paramètres 
+for (let i= 0; i< args.length; i++){
+  if(args[i] == '–action' && args[i+1] == 'transform'){
+    inputFile = args[i+2]
+    outputFile = args[i+3]
+    movieTransform (inputFile, outputFile)
+  }
 }
 
-let moviesData;
-fs.readFile('/Users/MaximeLuchini/Documents/GitHub/AlgoLV2/movies.json',{encoding: 'utf8'},function(err,data) {
-  moviesData = JSON.parse(data);
-  if(err) return console.error(err);
-  let countKey = Object.keys(moviesData).length;  
-for (i=0; i<countKey; i++) {
-  let moviesDate = moviesData[i]['release_date']
-  //let lir = new Date (moviesDate*1000).getFullYear
-  let lir = toDateTime(moviesDate)
-  let year = lir.getFullYear
-  console.log(year)
+//fonction permettant de prendre un fichier json, ajouter les années après le titre des films et afficher les films dans un fichier json de sortie
+function movieTransform (inputFile, outputFile){
+  let movieData;
+  //lire le fichier json d'entrée
+  fs.readFile('/Users/MaximeLuchini/Documents/GitHub/AlgoLV2/movies.json',{encoding: 'utf8'},function(err,data) {
+    movieData = JSON.parse(data);
+    if(err) return console.error(err);
+    let countKey = Object.keys(movieData).length;  
+      //parcourir chaque film et récupérer le release date pour le convertir en année
+      for (i=0; i<countKey; i++) {
+        let movieDate = new Date (movieData[i]['release_date'] * 1000)
+        let movieYear = movieDate.getFullYear();
 
-//console.log(moviesDate)
+        //nouveau titre avec année
+        movieData[i]['title'] = `${movieData[i]['title']} (${movieYear})`
+      }
+      
+      // Fonction d'enregistrement du fichier JSON après modifications des titres
+      let stringOut = JSON.stringify(movieData,null,'\t')
+      fs.writeFile(outputFile,stringOut,function(err) {
+      if(err)returnconsole.error(err);
+    })
+
+    
+    })
 }
-  })
-
-
-
-//     app.get('movies.json/all',sendAll);
- 
-//  function sendAll(request,response){
-//  response.send(words);
-
-//  }
